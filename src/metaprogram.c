094dae1a23439771294aae5da2eb1fa3a89c161d
TheSandvichMaker <49493579+TheSandvichMaker@users.noreply.github.com>
2021-02-07 03:33:47 +0100

    enum metaprogram party

--- src/metaprogram.c
+++ src/metaprogram.c
@@ -15,7 +15,7 @@ main(int argc, char** argv)
     fprintf(stderr, "Running metaprogram.\n");
 
     MD_FileInfo file_info;
-    for (MD_FileIter it = {}; MD_FileIterIncrement(&it, MD_S8Lit("*.mc"), &file_info);)
+    for (MD_FileIter it = { 0 }; MD_FileIterIncrement(&it, MD_S8Lit("*.mc"), &file_info);)
     {
         if (file_info.filename.size)
         {
@@ -42,9 +42,57 @@ main(int argc, char** argv)
         {
             for (MD_EachNode(node, root->first_child))
             {
-                if(MD_NodeHasTag(node, MD_S8Lit("struct")))
+                if (MD_NodeHasTag(node, MD_S8Lit("struct")))
                 {
                     MD_OutputTree_C_Struct(out_file, node);
+                }
+                else if (MD_NodeHasTag(node, MD_S8Lit("enum")))
+                {
+                    fprintf(out_file, "typedef enum test_enum;\n");
+                    fprintf(out_file, "enum test_enum\n{\n");
+                    for (MD_EachNode(enumeration, node->first_child))
+                    {
+                        fprintf(out_file, "    %.*s", MD_StringExpand(enumeration->string));
+                        if (enumeration->first_child)
+                        {
+                            MD_Expr *expr = MD_ParseAsExpr(enumeration->first_child, enumeration->last_child);
+                            MD_i64 value = MD_EvaluateExpr_I64(expr);
+                            fprintf(out_file, " = %lld", value);
+                        }
+                        fprintf(out_file, ",\n");
+                    }
+                    fprintf(out_file, "};\n\n");
+
+                    if (MD_NodeHasTag(node, MD_S8Lit("namefunc")))
+                    {
+                        fprintf(out_file, "static const char *\n%.*s_ToString(%.*s Value)\n{\n",
+                                MD_StringExpand(node->string),
+                                MD_StringExpand(node->string));
+                        fprintf(out_file, "    switch (Value)\n    {\n");
+                        for (MD_EachNode(enumeration, node->first_child))
+                        {
+                            fprintf(out_file, "        case %.*s: return \"%.*s\";\n",
+                                    MD_StringExpand(enumeration->string),
+                                    MD_StringExpand(enumeration->string));
+                        }
+                        fprintf(out_file, "    }\n");
+                        fprintf(out_file, "    return \"Unknown %.*s Value\";\n",
+                                MD_StringExpand(node->string));
+                        fprintf(out_file, "}\n\n");
+                    }
+
+                    if (MD_NodeHasTag(node, MD_S8Lit("nametable")))
+                    {
+                        fprintf(out_file, "const char *%.*s_Names[] =\n{\n",
+                                MD_StringExpand(node->string));
+                        for (MD_EachNode(enumeration, node->first_child))
+                        {
+                            fprintf(out_file, "    [%.*s] = \"%.*s\",\n",
+                                    MD_StringExpand(enumeration->string),
+                                    MD_StringExpand(enumeration->string));
+                        }
+                        fprintf(out_file, "}\n\n");
+                    }
                 }
             }
         }
